ed1824fbaef79179265e36301477e14775286c53
Semicolon <adedamola13@gmail.com>
2024-09-30 15:02:28 +0100

    updating the updates to this project

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -20,39 +20,7 @@ const HomePage = () =>{
 
                 
 
-                <h2 className="home-services">Services We Offer At <span className="seven-color">Seven star Hotel</span></h2>
-
-
-                <section className="service-section"><div className="service-card">
-                    <img src="./assets/images/ac.png" alt="Air-conditioning" />
-                    <div className="service-details">
-                        <h3 className="service-title">Air Conditioning</h3>
-                        <p className="service-description">Stay cool and comfortable throughout your stay with our individually controlled in-room air conditioning.</p>
-                    </div>
-                </div>
-                    <div className="service-card">
-                        <img src="./assets/images/mini-bar.png" alt ="Mini Bar" />
-                        <div className="service-details">
-                            <h3 className="service-title">Mini Bar</h3>
-                            <p className="service-description">Enjoy a convenient selection of beverages and snacks stocked in your room's mini bar with no additional cost. </p>
-                        </div>
-                    </div>
-                    <div className="service-card">
-                        <img src="./assets/images/parking.png" alt="parking" />
-                        <div className="service-details">
-                            <h3 className= "service-title">Parking</h3>
-                            <p className="service-description">We offer on-site spacious parking for your convenience . </p>
-                        </div>
-                    </div>
-                    <div className="service-card">
-                        <img src="./assets/images/wifi.png" alt="WIFI" />
-                        <div className="service-details">
-                            <h3 className="service-title">WIFI</h3>
-                            <p className="service-description"> Stay connected throughout your stay with complimentary high-speed Wi-Fi access available in all guest rooms and public areas.</p>
-                        </div>
-                    </div>
-
-                </section>
+                */
 
                 <section>
 
